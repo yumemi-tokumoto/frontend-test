e3afde81bef0e48aa1cbc4c18da29c468977cad9
Takashi Tokumoto <t_tokumoto@yumemi.co.jp>
2021-04-15 17:21:20 +0900

    q1

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { NextPage } from 'next'
 import { PageProps } from 'src/types'
-import { TextField } from 'src/components/input/TextField'
+import {
+  TextField,
+  Props as TextFieldProps,
+} from 'src/components/input/TextField'
 
 const HomePage: NextPage<PageProps> = () => {
   const [familyName, setFamilyName] = useState('')
   const [givenName, setGivenName] = useState('')
+
+  const onChangeFamilyName: TextFieldProps['onChange'] = useCallback(
+    (ev) => setFamilyName(ev.target.value),
+    [],
+  )
+  const onChangeGivenName: TextFieldProps['onChange'] = useCallback(
+    (ev) => setGivenName(ev.target.value),
+    [],
+  )
 
   return (
     <>
@@ -14,12 +26,12 @@ const HomePage: NextPage<PageProps> = () => {
         <span>姓</span>
         <TextField
           value={familyName}
-          onChange={(ev) => setFamilyName(ev.target.value)}
+          onChange={onChangeFamilyName}
         />
         <span>名</span>
         <TextField
           value={givenName}
-          onChange={(ev) => setGivenName(ev.target.value)}
+          onChange={onChangeGivenName}
         />
       </label>
       
