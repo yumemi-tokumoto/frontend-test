0e52887c47a58dbe852eb6838fe2bc2a38977e72
Takashi Tokumoto <t_tokumoto@yumemi.co.jp>
2021-04-15 16:51:44 +0900

    q1

---
+++ src/pages/sample.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { NextPage } from 'next'
+import { PageProps } from 'src/types'
+import { TextField } from 'src/components/input/TextField'
+
+const HomePage: NextPage<PageProps> = () => {
+  const [familyName, setFamilyName] = useState('')
+  const [givenName, setGivenName] = useState('')
+
+  return (
+    <>
+      <h1>サンプルページ</h1>
+      <label>
+        <span>姓</span>
+        <TextField
+          value={familyName}
+          onInput={(ev) => setFamilyName(ev.target.value)}
+        />
+        <span>名</span>
+        <TextField
+          value={givenName}
+          onInput={(ev) => setGivenName(ev.target.value)}
+        />
+      </label>
+      
+    </>
+  )
+}
+
+export default HomePage
